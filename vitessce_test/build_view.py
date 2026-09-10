f8283d4a6cf2cc8544b766f10abe362fe192976f
girochat <117285316+girochat@users.noreply.github.com>
2026-09-10 19:17:55 +0200

    feat(vitessce): improve view builder for spatial dataset
    Add preprocessing step to build SpatialData object saved as zarr with UMAP and
    clusters data. Support coordination between image and two segmentation layers
    (nucleus and cell boundaries). Synchronise gene expression data across viloin,
    scatterplot, cell boundaries and heatmap views.

--- vitessce_test/build_view.py
+++ vitessce_test/build_view.py
@@ -1,19 +1,34 @@
 import argparse
 import json
 from pathlib import Path
+from this import s
+from turtle import st
 from typing import Any
 
 import anndata as ad
-import yaml
-import zarr
-from vitessce import AnnDataWrapper, SpatialDataWrapper, VitessceConfig
+from numba.cuda.stubs import shared
+import pandas as pd
+import numpy as np
+from spatialdata_io import xenium
+from vitessce import (
+    AnnDataWrapper,
+    CoordinationLevel as CL,
+    SpatialDataWrapper,
+    VitessceConfig,
+    get_initial_coordination_scope_prefix,
+)
 from vitessce import Component as cm
 from vitessce import CoordinationType as ct
-from vitessce.config import VitessceConfigDataset, VitessceConfigView
+from vitessce.config import (
+    VitessceConfigDataset,
+    VitessceConfigView,
+    VitessceConfigCoordinationScope,
+)
+import yaml
 
 PORT = 8008
 BASE_URL = f"http://localhost:{PORT}"
-DATA_DIR = Path(".", "data", "Xenium_0OE1")
+DATA_DIR = Path(".", "data", "Dunlap_2022")
 
 DEFAULT_EMBEDDING_PATH = "obsm/X_umap"
 DEFAULT_EMBEDDING_NAME = "UMAP"
@@ -44,6 +59,8 @@ COORDINATION_MAPPING = {
     "featureHighlight": ct.FEATURE_HIGHLIGHT,
     "featureSelection": ct.FEATURE_SELECTION,
     "obsColorEncoding": ct.OBS_COLOR_ENCODING,
+    "featureValueColormapRange": ct.FEATURE_VALUE_COLORMAP_RANGE,
+    "spatialLayerColormap": "spatialLayerColormap",
 }
 
 
@@ -113,6 +130,7 @@ def add_anndata_dataset(
 
 def prepare_anndata_dataset(
     vc: VitessceConfig,
+    dataset_name: str,
     dataset: dict[str, Any]
 ) -> dict[str,VitessceConfigDataset]:
 
@@ -124,7 +142,7 @@ def prepare_anndata_dataset(
     if dataset['subsets_only'] and 'subsets' not in dataset:
         raise ValueError("subsets_only set to True but no subsets defined")
     if not dataset['subsets_only']:
-        datasets["A"] = dataset["dataset_name"], adata_path
+        datasets[dataset_name] = dataset_name, adata_path
     if 'subsets' in dataset:
         subsets = dataset["subsets"]
         for subset_id, subset in subsets.items():
@@ -134,7 +152,7 @@ def prepare_anndata_dataset(
                 subset["column"],
                 subset["value"]
             )
-            datasets[subset_id] = f"{dataset['dataset_name']} - {subset['value']}", subset_path
+            datasets[subset_id] = f"{dataset_name.capitalize()} - {subset['value']}", subset_path
 
     vitessce_datasets: dict[str, VitessceConfigDataset] = {}
     for dataset_id, dataset_data in datasets.items():
@@ -154,39 +172,43 @@ def prepare_anndata_dataset(
 
 def add_spatial_dataset(
     vc: VitessceConfig,
+    dataset_name: str,
     dataset: dict[str, Any],
     options: dict[str, Any],
 ) -> dict[str, VitessceConfigDataset]:
 
+    options = dataset.get("data", {})
     spatialzarr_path = Path(DATA_DIR, dataset["dataset_path"])
     image_path = "images/morphology_focus"
     obs_embedding_paths = options.get("obs_embedding_paths", ["tables/table/obsm/X_umap"])
     obs_embedding_names = options.get("obs_embedding_names", ["UMAP"])
     obs_sets_paths = options.get("obs_sets_paths", ["tables/table/obs/pred_cell_type"])
     obs_sets_names = options.get("obs_sets_names", ["Cell Type"])
-    obs_seg_paths = options.get("obs_seg_paths", {"Cell Boundaries":"shapes/cell_boundaries",
-        "Nucleus Boundaries": "shapes/nucleus_boundaries"})
+    obs_seg_paths = options.get("obs_seg_paths", ["shapes/cell_boundaries"])
     obs_feature_matrix_path = options.get("obs_feature_matrix_path", "tables/table/X")
-    obs_spots_paths = options.get("obs_spots_paths", "shapes/cell_circles")
-    obs_points_feature_index_column = None
-    obs_points_morton_code_column = None
+    obs_points_path = options.get("obs_points_path", "points/transcripts")
+    obs_points_feature_index_column = options.get("obs_points_feature_index_column", "codeword_index")
+    obs_points_morton_code_column = options.get("obs_points_morton_code_column", "morton_code_2d")
+    #obs_spots_path = options.get("obs_spots_path", "shapes/cell_circles")
 
     # Add datasets to the widget (dataset = container for file per data type)
-    vitessce_dataset = vc.add_dataset(                       # pyright: ignore[reportUnknownMemberType]
-        name=dataset["dataset_name"])
+    vitessce_dataset = vc.add_dataset(                        # pyright: ignore[reportUnknownMemberType]
+        name=dataset_name)
 
-    _ = vitessce_dataset.add_object(
+    _ = vitessce_dataset.add_object(                          # pyright: ignore[reportUnknownMemberType]
         SpatialDataWrapper(
             sdata_path=str(spatialzarr_path.relative_to(DATA_DIR)),
             image_path=image_path,
-            region="cell_circles",
-            coordinate_system="global",
-            #obs_embedding_paths=obs_embedding_paths,
-            #obs_embedding_names=obs_embedding_names,
+            obs_embedding_paths=obs_embedding_paths,
+            obs_embedding_names=obs_embedding_names,
             obs_set_paths=obs_sets_paths,
             obs_set_names=obs_sets_names,
             obs_feature_matrix_path=obs_feature_matrix_path,
-            obs_spots_path=obs_spots_paths,
+            #obs_points_path=obs_points_path,
+            #obs_points_feature_index_column=obs_points_feature_index_column,
+            #obs_points_morton_code_column=obs_points_morton_code_column,
+            #obs_spots_path=obs_spots_path,
+            #obs_segmentations_path=obs_seg_path,
             coordination_values={
                 "obsType": "cell",
                 "featureType": "gene",
@@ -195,18 +217,65 @@ def add_spatial_dataset(
         )
     )
 
-    for seg_name, seg_path in obs_seg_paths.items():
-        continue
+    for seg_path in obs_seg_paths:
         url = f"{BASE_URL}/{spatialzarr_path.relative_to(DATA_DIR).as_posix()}"
-        _ = vitessce_dataset.add_file(
+        if 'cell' in seg_path:
+            seg_type = "cell"
+        elif 'nucleus' in seg_path:
+            seg_type = "nucleus"
+        else:
+            seg_type = "cell"
+        _ = vitessce_dataset.add_file(                          # pyright: ignore[reportUnknownMemberType]
             file_type = "shapes.spatialdata.zarr",
             url=url,
-            coordination_values=None,
+            coordination_values={
+                "obsType": seg_type,
+                "fileUid": seg_type
+            },
             options={
-                "path": seg_path
+                "path": seg_path,
+                "tablePath": "tables/table",
+                "region": "cell_boundaries"
             }
         )
-    return { dataset["dataset_name"]: vitessce_dataset }
+    return { dataset_name: vitessce_dataset }
+
+
+def prepare_spatial_dataset(
+    vc: VitessceConfig,
+    dataset_name: str,
+    dataset: dict[str, Any],
+) -> dict[str, VitessceConfigDataset]:
+
+    dataset_path = Path(DATA_DIR, dataset["dataset_path"])
+    if not dataset.get("is_preprocessed", False) and not dataset_path.with_suffix(".zarr").exists():
+        sdata = xenium(dataset_path)
+        adata = sdata.tables["table"]
+        umap_data = pd.read_csv(dataset_path / "analysis" / "umap" / "gene_expression_2_components" / "projection.csv", index_col=0)
+        umap_data_full = pd.DataFrame(index=adata.obs["cell_id"])
+        umap_data_full.loc[umap_data.index, ["UMAP-1", "UMAP-2"]] = umap_data.loc[umap_data.index, ["UMAP-1", "UMAP-2"]]
+        adata.obsm["X_umap"] = umap_data_full.to_numpy(np.float32)
+        clusters = pd.read_csv(dataset_path / "analysis" / "clustering" / "gene_expression_graphclust" / "clusters.csv", index_col=0)
+        clusters_full = pd.DataFrame(index=adata.obs["cell_id"])
+        clusters_full.loc[clusters.index, "Cluster"] = clusters.loc[clusters.index, "Cluster"]
+        adata.obs["clusters"] = clusters_full["Cluster"]
+
+        # Handle nullable string indexes for compatibility with older pandas versions.
+        adata.var.index = pd.Index(
+            adata.var.index.astype(str).to_numpy(),
+            dtype=object,
+        )
+        adata.obs.index = pd.Index(
+            adata.obs.index.astype(str).to_numpy(),
+            dtype=object,
+        )
+        ad.settings.allow_write_nullable_strings=False
+
+        sdata.tables["table"] = adata
+        sdata.write(dataset_path.with_suffix(".zarr"))
+        dataset["dataset_path"] = str(dataset_path.relative_to(DATA_DIR).with_suffix(".zarr"))
+
+    return add_spatial_dataset(vc, dataset_name, dataset, options=dataset.get("data", {}))
 
 
 def add_view(
@@ -235,27 +304,104 @@ def add_view(
         ).set_xywh(x, y, w, h)
 
     if props is not None:
-        view = view.set_props(**props)
+        view = view.set_props(**props)              # pyright: ignore[reportUnknownMemberType]
 
     return view
+
+
+def add_image_layer_coordination(
+    vc: VitessceConfig,
+    views: list[VitessceConfigView],
+    values: str,
+) -> None:
+    _ = vc.link_views_by_dict(                              # pyright: ignore[reportUnknownMemberType]
+        views,
+        {
+            "imageLayer": CL([
+                values
+            ])
+        },
+        scope_prefix=get_initial_coordination_scope_prefix(
+            "A",
+            "image",
+        ),
+    )
+
+
+def add_segmentation_layer_coordination(
+    vc: VitessceConfig,
+    views: list[VitessceConfigView],
+    layers_data: dict[str, Any],
+) -> None:
+    layers = [
+        {
+            "fileUid": channel.replace("_channel", ""),
+            "segmentationChannel": CL([
+                layers_data[channel]
+            ]),
+        } for channel in layers_data
+    ]
+    if not layers:
+        return
+    _ = vc.link_views_by_dict(                              # pyright: ignore[reportUnknownMemberType]
+        views,
+        {
+            "segmentationLayer": CL([*layers])
+        },
+        scope_prefix=get_initial_coordination_scope_prefix(
+            "A",
+            "obsSegmentations",
+        ),
+    )
 
 
 def add_coordination_space(
     vc: VitessceConfig,
     coordination_space: dict[str, Any],
     views: dict[str, dict[str, VitessceConfigView]],
-) -> None:
-    # Define per condition coordination scope
+) -> dict[str, VitessceConfigCoordinationScope]:
+
+    # Define views to link per dataset
     datasets = coordination_space["datasets"]
     view_to_link_names = coordination_space["views"]
-    vitessce_view_to_link = [views[dataset][view_name] for dataset in datasets for view_name in view_to_link_names]
-    coord_types = [COORDINATION_MAPPING[coord_type] for coord_type in coordination_space["values"]]
-    coord_initial_values = [coordination_space["values"][coord_type] for coord_type in coordination_space["values"]]
-    _ = vc.link_views(                              # pyright: ignore[reportUnknownMemberType]
-        vitessce_view_to_link,
-        coord_types,
-        coord_initial_values,
-    )
+
+    scopes: dict[str, VitessceConfigCoordinationScope] = {}
+
+    # Keep track of the created coordination scopes
+    for coord_name, initial_value in coordination_space["values"].items():
+        coord_type = COORDINATION_MAPPING[coord_name]
+        scope: VitessceConfigCoordinationScope = vc.add_coordination(coord_type)[0]   # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
+        _ = scope.set_value(initial_value)                                            # pyright: ignore[reportUnknownMemberType]
+        scopes[coord_name] = scope
+
+        for dataset in datasets:
+            for view_name in view_to_link_names:
+                view = views[dataset][view_name]
+                _ = view.use_coordination(scope)           # pyright: ignore[reportUnknownMemberType]
+
+    # Add specific layer coordination spaces for each spatial dataset (required by spatial and layer controller
+    # views)
+    if "per_layer" in coordination_space:
+        for dataset in datasets:
+            for layer_name, layer_data in coordination_space["per_layer"].items():
+                view_to_link_names = layer_data["views"]
+                views_to_link = [views[dataset][view_name]
+                    for view_name in view_to_link_names]
+                if layer_name == "image_layer":
+                    add_image_layer_coordination(vc, views_to_link, layer_data["values"])
+                if layer_name == "segmentation_layer":
+                    layers = {channel: None for channel in ["cell_channel", "nucleus_channel"] if channel in layer_data}
+                    if not layers:
+                        continue
+                    for channel in layers:
+                        if "from" in layer_data[channel]["values"]:
+                            layers[channel] = scopes
+                            layer_data[channel]["values"].pop("from")
+                            layers[channel].update(layer_data[channel]["values"])
+                        else:
+                            layers[channel] = layer_data[channel]["values"]
+                    add_segmentation_layer_coordination(vc, views_to_link, layers)
+    return scopes
 
 
 def build_view(
@@ -269,17 +415,20 @@ def build_view(
         description=template["description"],
         base_dir=DATA_DIR)
 
-    # Record all datasets
-    datasets = [template["datasets"][dataset] for dataset in template["datasets"]]
+    # Record all datasets to display
+    datasets = template["datasets"]
 
-    # Add datasets to config
+    # Add datasets to vitessce config
     vitessce_datasets: dict[str, VitessceConfigDataset] = {}
-    for dataset in datasets:
+    spatial_datasets: dict[str, VitessceConfigDataset] = {}
+    for dataset_name, dataset in datasets.items():
         if dataset["type"] == "anndata_zarr":
-            vitessce_datasets.update(prepare_anndata_dataset(vc, dataset))
+            vitessce_datasets.update(prepare_anndata_dataset(vc, dataset_name, dataset))
         if dataset["type"] == "spatialdata_zarr":
-            vitessce_datasets.update(add_spatial_dataset(vc, dataset, options=dataset["data"]))
+            vitessce_datasets.update(prepare_spatial_dataset(vc, dataset_name, dataset))
+            spatial_datasets[dataset_name] = vitessce_datasets[dataset_name]
 
+    # Add views to vitessce config
     views = template["views"]
     views_per_dataset: dict[str, dict[str, VitessceConfigView]] = {}
     for vitessce_dataset_name, vitessce_dataset in vitessce_datasets.items():
@@ -304,9 +453,10 @@ def build_view(
                 props=views[view].get("props", None),
             )
 
-    coordination_spaces = [template["coordination"][space] for space in template["coordination"]]
-    for coordination_space in coordination_spaces:
-        add_coordination_space(vc, coordination_space, views_per_dataset)
+    # Coordinate views across datasets
+    coordination_scopes: dict[str, dict[str, Any]] = {}
+    for space_name, coordination_space in template["coordination"].items():
+        coordination_scopes[space_name] = add_coordination_space(vc, coordination_space, views_per_dataset)
 
     _ = vc.web_app(port=PORT)                               # pyright: ignore[reportUnknownMemberType]
     return vc.to_dict(base_url=BASE_URL)                    # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
