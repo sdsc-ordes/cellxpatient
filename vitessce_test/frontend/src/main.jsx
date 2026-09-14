69f55dacece9491671f0f01d1a7eef9c46ac4acc
girochat <117285316+girochat@users.noreply.github.com>
2026-09-14 13:16:47 +0200

    chore(vitessce,frontend): match referenced react and vite version for latest vitessce

--- vitessce_test/frontend/src/main.jsx
+++ vitessce_test/frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'       => temporarily deactivate due to rendering issues in dev mode
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //<StrictMode>
     <App />
-  </StrictMode>,
+  //</StrictMode>,
 )
